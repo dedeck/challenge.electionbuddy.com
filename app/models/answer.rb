29fa9e17aa22be21510b27da42d7f88c5d29e103
Adam <dedeck17@gmail.com>
2020-11-17 20:56:42 -0700

    add audit to answer, question, voter

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,18 @@
 class Answer < ApplicationRecord
   belongs_to :question
   has_many :audits, as: :changed_attribute
+
+  before_save :add_audit
+
+  def add_audit
+    if self.changed?
+      timestamp = Time.now
+      self.changed.each do |attribute|
+        Audit.create(election: self.question.election, changed_attribute: self, value_changed: attribute,
+                     previous_value: self.changed_attributes[attribute],
+                     new_value: self.attributes[attribute],changed_timestamp: timestamp,
+                     user_id: Current.user.id)
+      end
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,18 @@ class Question < ApplicationRecord
   belongs_to :election
   has_many :answers
   has_many :audits, as: :changed_attribute
+
+  before_save :add_audit
+
+  def add_audit
+    if self.changed?
+      timestamp = Time.now
+      self.changed.each do |attribute|
+        Audit.create(election: self.election, changed_attribute: self, value_changed: attribute,
+                     previous_value: self.changed_attributes[attribute],
+                     new_value: self.attributes[attribute],changed_timestamp: timestamp,
+                     user_id: Current.user.id)
+      end
+    end
+  end
 end
