365c05a84863eba465717e2e5421c68683297c9c
Adam <dedeck17@gmail.com>
2020-11-17 20:42:57 -0700

    order by most recent change first

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -2,7 +2,7 @@ class AuditsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_election
   def index
-    @audits = Audit.where(election: @election)
+    @audits = Audit.where(election: @election).order(created_at: :desc)
   end
 
   def set_election
